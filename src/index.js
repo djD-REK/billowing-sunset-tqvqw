66c82034dd87f11be1325777783474506efb7e20
Dr. Derek Austin <53202242+djD-REK@users.noreply.github.com>
2019-09-09 23:20:25 -0400

    Added prettier config, removed semicolons, & reviewed code before submit

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import "./styles.css";
+import "./styles.css"
 
 document.getElementById("app").innerHTML = `
 <h1>Hello RTS Labs!</h1>
@@ -6,7 +6,7 @@ document.getElementById("app").innerHTML = `
   Thanks for the opportunity to interview!
 </p>
 <h4>Best regards,<br/>Derek</h4>
-`;
+`
 
 /**
  * Question 1 - Derek Austin
@@ -20,10 +20,10 @@ console.log(`/**
 * Print the number of integers in an array that are above the
 * given input and the number that are below, e.g. for the array
 * [1, 5, 2, 1, 10] with input 6, print "above: 1, below: 4"
-*/`);
+*/`)
 
-const array = [1, 5, 2, 1, 10];
-const inputThreshold = 6;
+const array = [1, 5, 2, 1, 10]
+const inputThreshold = 6
 /**
  * Returns a string in the format "above: #, below: #"
  * based on the number of integers in an array that
@@ -40,20 +40,20 @@ const aboveAndBelow = (array, inputThreshold) => {
       // Add 1 to the accumulator if the value is above the input threshold
       currentValue > inputThreshold ? (accumulator += 1) : accumulator,
     0
-  );
+  )
   // Count the number of integers below the threshold using reduce to check each element
   const below = array.reduce(
     (accumulator, currentValue) =>
       // Add 1 to the accumulator if the value is below the input threshold
       currentValue < inputThreshold ? (accumulator += 1) : accumulator,
     0
-  );
-  return `above: ${above}, below: ${below}`;
-};
+  )
+  return `above: ${above}, below: ${below}`
+}
 
 console.log(`*** Implementation using reduce ***
 aboveAndBelow([${array}], ${inputThreshold})
-Result: "${aboveAndBelow(array, inputThreshold)}"`);
+Result: "${aboveAndBelow(array, inputThreshold)}"`)
 // Result: "above: 1, below: 4"
 
 /**
@@ -67,22 +67,22 @@ Result: "${aboveAndBelow(array, inputThreshold)}"`);
  */
 const aboveAndBelowMap = (array, inputThreshold) => {
   // Create an accumulator object to keep our counts
-  const accumulator = { above: 0, below: 0 };
+  const accumulator = { above: 0, below: 0 }
   // Count the number of integers above and below the threshold using map to check each value
   array.map(value => {
     if (value > inputThreshold) {
       // Add 1 to the accumulator if the value is above the input threshold
-      accumulator.above += 1;
+      accumulator.above += 1
     } else if (value < inputThreshold) {
       // Add 1 to the accumulator if the value is below the input threshold
-      accumulator.below += 1;
+      accumulator.below += 1
     }
-  });
-  return `above: ${accumulator.above}, below: ${accumulator.below}`;
-};
+  })
+  return `above: ${accumulator.above}, below: ${accumulator.below}`
+}
 console.log(`*** Implementation using reduce ***
 aboveAndBelowMap([${array}], ${inputThreshold})
-Result: "${aboveAndBelowMap(array, inputThreshold)}"`);
+Result: "${aboveAndBelowMap(array, inputThreshold)}"`)
 // Result: "above: 1, below: 4"
 
 /**
@@ -96,38 +96,38 @@ Result: "${aboveAndBelowMap(array, inputThreshold)}"`);
  */
 const aboveAndBelowForLoop = (array, inputThreshold) => {
   // Create our counter variables
-  let aboveThreshold = 0;
-  let belowThreshold = 0;
+  let aboveThreshold = 0
+  let belowThreshold = 0
   // Iterate over the array using a for loop to count the number of integers above and below the threshold
   for (let i = 0; i < array.length; i++) {
-    let value = array[i];
+    let value = array[i]
     if (value > inputThreshold) {
       // Add 1 to the accumulator if the value is above the input threshold
-      aboveThreshold += 1;
+      aboveThreshold += 1
     } else if (value < inputThreshold) {
       // Add 1 to the accumulator if the value is below the input threshold
-      belowThreshold += 1;
+      belowThreshold += 1
     }
   }
-  return `above: ${aboveThreshold}, below: ${belowThreshold}`;
-};
+  return `above: ${aboveThreshold}, below: ${belowThreshold}`
+}
 console.log(`*** Implementation using for loop ***
 aboveAndBelowForLoop([${array}], ${inputThreshold})
-Result: "${aboveAndBelowForLoop(array, inputThreshold)}"`);
+Result: "${aboveAndBelowForLoop(array, inputThreshold)}"`)
 // Result: "above: 1, below: 4"
 
 // Unit test:
-console.log(`*** Unit test ***`);
-(function unitTest() {
-  const story = `Count numbers above and below 6 in [1, 5, 2, 1, 10]`;
-  const expectation = "above: 1, below: 4";
-  const output = aboveAndBelow([1, 5, 2, 1, 10], 6);
-  const result = output === expectation ? "✓PASS" : "✗FAIL";
+console.log(`*** Unit test ***`)
+;(function unitTest() {
+  const story = `Count numbers above and below 6 in [1, 5, 2, 1, 10]`
+  const expectation = "above: 1, below: 4"
+  const output = aboveAndBelow([1, 5, 2, 1, 10], 6)
+  const result = output === expectation ? "✓PASS" : "✗FAIL"
   console.log(
     `Test: ${result} ${story}
             ${output} should match ${expectation}`
-  );
-})(); // Encapsulated as an IIFE so the function runs automatically
+  )
+})() // Encapsulated as an IIFE so the function runs automatically
 
 /**
  * Question 2 - Derek Austin
@@ -141,7 +141,7 @@ console.log(`/**
 * Rotate the characters in a string by a given input and
 * have the overflow appear at the beginning, e.g.
 * “MyString” rotated by 2 is “ngMyStri”.
-*/`);
+*/`)
 
 /**
  * Returns the input string rotated by the specified
@@ -156,15 +156,15 @@ console.log(`/**
 const rotateString = (inputString = "", howManyCharacters = 0) => {
   // Optional type checking:
   // if (inputString == null || typeof inputString !== "string") return "";
-  const length = inputString.length;
-  const overflow = inputString.substring(howManyCharacters, length);
-  const remainder = inputString.substring(0, howManyCharacters);
-  const rotatedString = overflow + remainder;
-  return rotatedString;
-};
+  const length = inputString.length
+  const overflow = inputString.substring(howManyCharacters, length)
+  const remainder = inputString.substring(0, howManyCharacters)
+  const rotatedString = overflow + remainder
+  return rotatedString
+}
 console.log(`*** Basic function ***
 rotateString("MyString", 3)
-Result: "${rotateString("MyString", 3)}"`);
+Result: "${rotateString("MyString", 3)}"`)
 // Result "tringMyS"
 
 /**
@@ -179,18 +179,18 @@ Result: "${rotateString("MyString", 3)}"`);
  */
 const rotateStringCoercion = (inputString = "", howManyCharacters = 0) => {
   // Force type coercion to String by using the + operator to add the empty string
-  inputString += "";
+  inputString += ""
   // Alternatively, for readability: let definitelyString = String(inputString);
   return (
     inputString.substring(howManyCharacters, inputString.length) +
     inputString.substring(0, howManyCharacters)
-  );
-};
+  )
+}
 console.log(`*** Condensed function, with type coercion ***
 rotateStringCoercion("MyString", 3)
 Result: "${rotateStringCoercion("MyString", 3)}"
 rotateStringCoercion(3)
-Result: "${rotateStringCoercion(3)}"`);
+Result: "${rotateStringCoercion(3)}"`)
 // Results: "tringMyS" and "3"
 
 /**
@@ -208,35 +208,35 @@ const rotateStringOneLine = (inputString = "", howManyCharacters = 0) =>
   inputString == null || typeof inputString !== "string"
     ? "" // Either null or not a string type, so return the empty string
     : inputString.substring(howManyCharacters, inputString.length) +
-      inputString.substring(0, howManyCharacters); // Return the rotated string
+      inputString.substring(0, howManyCharacters) // Return the rotated string
 console.log(`*** One line function, with type checking ***
 rotateStringOneLine("MyString", 3)
 Result: "${rotateStringOneLine("MyString", 3)}"
 rotateStringOneLine(3)
-Result: "${rotateStringOneLine(3)}"`);
+Result: "${rotateStringOneLine(3)}"`)
 // Results: "tringMyS" and ""
 
 // Unit test:
 const unitTest = () => {
   // Anonymous function assigned to the const unitTest
-  console.log(`*** Unit test ***`);
-  const story = `Rotate string by 2 characters`;
-  const input = "MyString";
-  const expectation = "StringMy";
-  const output = rotateString(input, 2);
-  const result = output === expectation ? "✓PASS" : "✗FAIL";
+  console.log(`*** Unit test ***`)
+  const story = `Rotate string by 2 characters`
+  const input = "MyString"
+  const expectation = "StringMy"
+  const output = rotateString(input, 2)
+  const result = output === expectation ? "✓PASS" : "✗FAIL"
   console.log(`Test: ${result} ${story}
-            ${output} should match ${expectation}`);
-};
+            ${output} should match ${expectation}`)
+}
 // Run unit test for rotateString
-unitTest();
+unitTest()
 
 // Expanded unit tests:
-console.log(`*** Expanded unit tests ***`);
+console.log(`*** Expanded unit tests ***`)
 const runTestSuite = function generateUnitTests() {
   // Using a named function for maximum code readability
-  const input = "MyString";
-  const possibleRotations = Array.from(Array(input.length + 1).keys()); // Generate array of possible rotations up to input's length
+  const input = "MyString"
+  const possibleRotations = Array.from(Array(input.length + 1).keys()) // Generate array of possible rotations up to input's length
   const expectations = [
     "MyString",
     "yStringM",
@@ -247,19 +247,19 @@ const runTestSuite = function generateUnitTests() {
     "ngMyStri",
     "gMyStrin",
     "MyString"
-  ];
+  ]
   possibleRotations.map(howFar => {
-    const story = `Rotate string by ${howFar} characters`;
-    const input = "MyString";
-    const output = rotateString(input, howFar);
-    const result = output === expectations[howFar] ? "✓PASS" : "✗FAIL";
+    const story = `Rotate string by ${howFar} characters`
+    const input = "MyString"
+    const output = rotateString(input, howFar)
+    const result = output === expectations[howFar] ? "✓PASS" : "✗FAIL"
     console.log(
       `Test: ${result} ${story}: ${output} should match ${expectations[howFar]}`
-    );
-  });
-};
+    )
+  })
+}
 // Run test suite for rotateString
-runTestSuite();
+runTestSuite()
 
 /**
  * Question 3 - Derek Austin
@@ -271,9 +271,9 @@ console.log(`/**
 * Question 3 - Derek Austin
 * If you could change 1 thing about your favorite
 * framework/language/platform (pick one), what would it be?
-*/`);
+*/`)
 
 console.log(`I have a personal preference (probably from Ruby) against
 semicolons, so I would love for JavaScript developers to agree:
 let’s all stop using semicolons and update our prettier configs.
--Derek 😁`);
+-Derek 😁`)
